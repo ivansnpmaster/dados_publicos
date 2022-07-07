de2feae804d057e9d893e6b6bdf48f8fd98e8b9d
ivansnpmaster <ivansnpmaster@gmail.com>
2022-07-07 15:59:10 -0300

    2020 - renomeando arquivos

--- particionar.py
+++ particionar.py
@@ -5,4 +5,4 @@ i = 0
 with pd.read_csv("MICRODADOS_ENEM_2020.csv", encoding = "ISO-8859-1", sep = ';', chunksize = chunksize) as reader:
     for chunk in reader:
         i += 1
-        chunk.to_parquet('2020/MICRODADOS_ENEM_2020_{}.parquet'.format(i))
+        chunk.to_parquet('2020/MICRODADOS_ENEM_2020_{}.parquet'.format("0" + str(i) if len(str(i)) == 1 else i))
