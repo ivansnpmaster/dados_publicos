2eea023e234ae2e42a847a846917a4d625c6d746
ivansnpmaster <ivansnpmaster@gmail.com>
2022-07-07 15:42:17 -0300

    2020 - chunks menores

--- particionar.py
+++ particionar.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import pyarrow as pa
 
-chunksize = 900000
-i = 1
-with pd.read_csv("MICRODADOS_ENEM_2020.csv", encoding="ISO-8859-1", sep = ';', chunksize=chunksize) as reader:
+chunksize = 400000
+i = 0
+with pd.read_csv("MICRODADOS_ENEM_2020.csv", encoding = "ISO-8859-1", sep = ';', chunksize = chunksize) as reader:
     for chunk in reader:
-        chunk.to_parquet('MICRODADOS_ENEM_2020_{}.parquet'.format(i))
         i += 1
+        chunk.to_parquet('2020/MICRODADOS_ENEM_2020_{}.parquet'.format(i))
